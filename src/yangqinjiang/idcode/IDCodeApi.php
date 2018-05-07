2a838796db7d2db77295b1ca0141ff094c44acc4
yangqinjiang <531553280@qq.com>
2018-05-07 16:13:59 +0800

    101,102,103接口完善

--- src/yangqinjiang/idcode/IDCodeApi.php
+++ src/yangqinjiang/idcode/IDCodeApi.php
@@ -11,10 +11,86 @@ namespace yangqinjiang\idcode;
 
 class IDCodeApi
 {
-    //一次性返回所有级别行政信息
+    //101:一次性返回所有级别行政信息
     public static function addresses()
     {
-        
+        $data = new \yangqinjiang\idcode\data\IDcodeDataBase();
+        //测试数据
+        $hash = $data
+            ->setUrl('http://apitest.idcode.org.cn')
+            ->setAccessToken('J7X4LuOOfURXeAAEAAAAAABSK')//系统授权key
+            ->setSysAuthCode('RORJOOfU4OJ44XUf4uuXU4JuRLeeRAX7')//系统授权码
+            ->setTime(time()*1000)
+            ->setPath('/sp/idcode/addresses')
+            ->SetHash()
+            ->GetHash();
+        $url = $data->rquest_url;
+
+        $client = new \GuzzleHttp\Client();
+
+        $res = $client->request('GET', $url);
+        return json_decode($res->getBody(),true);
     }
 
+    //102:按照父级 ID 返回子级信息
+    public static function address_id_parent($id_parent,$level)
+    {
+        $data = new \yangqinjiang\idcode\data\IDcodeDataBase();
+        //测试数据
+        $hash = $data
+            ->setUrl('http://apitest.idcode.org.cn')
+            ->setAccessToken('J7X4LuOOfURXeAAEAAAAAABSK')//系统授权key
+            ->setSysAuthCode('RORJOOfU4OJ44XUf4uuXU4JuRLeeRAX7')//系统授权码
+            ->setTime(time()*1000)
+            ->setPath('/sp/idcode/addresses/parent/id')
+            ->setOne('address_id_parent',$id_parent)
+            ->setOne('level',$level)
+            ->SetHash()
+            ->GetHash();
+        $url = $data->rquest_url;
+
+        $client = new \GuzzleHttp\Client();
+        $res = $client->request('GET', $url);
+        return json_decode($res->getBody(),true);
+    }
+
+    //103:一次性返回所有级别行业信息
+    public static function trades()
+    {
+        $data = new \yangqinjiang\idcode\data\IDcodeDataBase();
+        //测试数据
+        $hash = $data
+            ->setUrl('http://apitest.idcode.org.cn')
+            ->setAccessToken('J7X4LuOOfURXeAAEAAAAAABSK')//系统授权key
+            ->setSysAuthCode('RORJOOfU4OJ44XUf4uuXU4JuRLeeRAX7')//系统授权码
+            ->setTime(time()*1000)
+            ->setPath('/sp/idcode/trades')
+            ->SetHash()
+            ->GetHash();
+        $url = $data->rquest_url;
+
+        $client = new \GuzzleHttp\Client();
+        $res = $client->request('GET', $url);
+        return json_decode($res->getBody(),true);
+    }
+    //104:一按照父级ID返回子级信息
+    public static function trades_id_parent($trade_id_parent)
+    {
+        $data = new \yangqinjiang\idcode\data\IDcodeDataBase();
+        //测试数据
+        $hash = $data
+            ->setUrl('http://apitest.idcode.org.cn')
+            ->setAccessToken('J7X4LuOOfURXeAAEAAAAAABSK')//系统授权key
+            ->setSysAuthCode('RORJOOfU4OJ44XUf4uuXU4JuRLeeRAX7')//系统授权码
+            ->setTime(time()*1000)
+            ->setPath('/sp/idcode/trades/parent/id')
+            ->setOne('trade_id_parent',$trade_id_parent)
+            ->SetHash()
+            ->GetHash();
+        $url = $data->rquest_url;
+
+        $client = new \GuzzleHttp\Client();
+        $res = $client->request('GET', $url);
+        return json_decode($res->getBody(),true);
+    }
 }
--- tests/HttpBaseTest.php
+++ tests/HttpBaseTest.php
@@ -14,7 +14,7 @@ class HttpBaseTest extends \PHPUnit\Framework\TestCase
         $data = new \yangqinjiang\idcode\data\IDcodeDataBase();
         //测试数据
         $hash = $data
-            ->setUrl('https://apitest.idcode.org.cn')
+            ->setUrl('http://apitest.idcode.org.cn')
             ->setPath('/abc/def')
             ->setAccessToken('J7X4LuOOfURXeAAEAAAAAABSK')//系统授权key
             ->setSysAuthCode('RORJOOfU4OJ44XUf4uuXU4JuRLeeRAX7')//系统授权码
@@ -27,6 +27,46 @@ class HttpBaseTest extends \PHPUnit\Framework\TestCase
         $url = $data->rquest_url;
 
         $this->assertEquals('4E5B67B75447486AFC147BE5DEBF7758', $hash);
-        $this->assertEquals('https://apitest.idcode.org.cn/abc/def?access_token=J7X4LuOOfURXeAAEAAAAAABSK&id=123&name=zhangsan&time=1497587696789&hash=4E5B67B75447486AFC147BE5DEBF7758', $url);
+        $this->assertEquals('http://apitest.idcode.org.cn/abc/def?access_token=J7X4LuOOfURXeAAEAAAAAABSK&id=123&name=zhangsan&time=1497587696789&hash=4E5B67B75447486AFC147BE5DEBF7758', $url);
+    }
+
+    public function testAddresses()
+    {
+        $json =  \yangqinjiang\idcode\IDCodeApi::addresses();
+        $this->assertArrayHasKey('address_list',$json);
+        $this->assertArrayNotHasKey('not_this_array',$json);
+        $this->assertEquals('1',$json['result_code']);
+    }
+    public function testAddressesIDParent()
+    {
+        //广东省,区域等级 1, 即返回广东省,一个数据
+        $json =  \yangqinjiang\idcode\IDCodeApi::address_id_parent(440000,1);
+
+        $this->assertArrayHasKey('address_list',$json);
+        $this->assertEquals(1,count($json['address_list']));
+        $this->assertEquals(440000,$json['address_list'][0]['address_code']);
+        $this->assertArrayNotHasKey('not_this_array',$json);
+        $this->assertEquals('1',$json['result_code']);
+
+        //广东省,区域等级 2, 有20个地级市
+        $json =  \yangqinjiang\idcode\IDCodeApi::address_id_parent(440000,2);
+        $this->assertArrayHasKey('address_list',$json);
+        $this->assertTrue(count($json['address_list']) > 19);
+        $this->assertNotTrue(count($json['address_list']) > 100);
+    }
+
+    public function testTrades()
+    {
+        $json =  \yangqinjiang\idcode\IDCodeApi::trades();
+
+        $this->assertArrayHasKey('trade_list',$json);
+        $this->assertEquals(1,$json['result_code']);
+
+        //$trade_id_parent = 5000 景点
+        $json =  \yangqinjiang\idcode\IDCodeApi::trades_id_parent(5000);
+
+        $this->assertArrayHasKey('trade_list',$json);
+        $this->assertEquals(1,$json['result_code']);
+
     }
 }
